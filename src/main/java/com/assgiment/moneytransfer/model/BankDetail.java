bf3990e68b3129a7ff6fadb38f49078db237c4ae
Suresh <>
2021-03-21 09:17:31 +0400

    Removed non Used hashcode Equals method

--- src/main/java/com/assgiment/moneytransfer/model/BankDetail.java
+++ src/main/java/com/assgiment/moneytransfer/model/BankDetail.java
@@ -32,41 +32,4 @@ public class BankDetail {
 	@OneToOne(cascade = CascadeType.ALL)
 	private Address branchAddress;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((branchAddress == null) ? 0 : branchAddress.hashCode());
-		result = prime * result + ((branchCode == null) ? 0 : branchCode.hashCode());
-		result = prime * result + ((branchName == null) ? 0 : branchName.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		BankDetail other = (BankDetail) obj;
-		if (branchAddress == null) {
-			if (other.branchAddress != null)
-				return false;
-		} else if (!branchAddress.equals(other.branchAddress))
-			return false;
-		if (branchCode == null) {
-			if (other.branchCode != null)
-				return false;
-		} else if (!branchCode.equals(other.branchCode))
-			return false;
-		if (branchName == null) {
-			if (other.branchName != null)
-				return false;
-		} else if (!branchName.equals(other.branchName))
-			return false;
-		return true;
-	}
-
 }
